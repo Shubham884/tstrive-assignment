8c22218afba8ffbc594d4267c0f0c0c55f31864f
Shubham <shubham.manjup@gmail.com>
2021-10-25 11:37:41 +0530

    Fix compilation error by importing correct component

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Tree from '../src/components/Tree/index'
+import Tree from '../src/components/Menu'
+import AccordionTree from 'components/AccordionTree';
 
 function App() {
   return (
     <div className="App">
+      <div>Component 1</div>
       <Tree />
+      <div>Component 2</div>
+      <AccordionTree />
     </div>
   );
 }
